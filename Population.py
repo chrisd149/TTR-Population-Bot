8cc34c8217a6fd449e4d3a470d639490d3deb331
chrisd149 <christianmigueldiaz@gmail.com>
2020-01-04 15:47:10 -0500

    Population: Added population stuff to Bot.py

--- Population.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# this requests the population api, and formats it into the data we need (i.e. the population)
-population_api = 'https://www.toontownrewritten.com/api/population'
-
-request = requests.get(url=population_api, params=None)
-
-data = request.json()
-population = data['totalPopulation']  # formats data to only include population data
