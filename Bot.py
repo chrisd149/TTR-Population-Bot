6209884e18b0693b6a3bb06317343f938d907a10
Christian Diaz <48182689+chrisd149@users.noreply.github.com>
2020-01-06 00:19:35 -0500

    Grapher:  Added graphing function

--- Bot.py
+++ Bot.py
@@ -4,49 +4,109 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 import logging
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics
+
+startTime = datetime.today()
+
+population_list = []
+
+headers = {
+    'User-Agent': 'TTR Population Tracker(@TTR_Population) (A twitter bot run by @miguel_TTR) GitHub repository: '
+                  'https://github.com/chrisd149/Toontown-Rewritten-API-Bot',
+    'From': 'christianmigueldiaz@gmail.com'
+}
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 # You must make a Keys.py with the keys in the auth variables
-Time = datetime.datetime.now()
+Time = datetime.now()
 current_time = Time.strftime('%H:%M')
-
+current_day = Time.strftime('%A, %B %d')
+print(current_day)
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-logger.info(F"Starting bot at {current_time}")
-
-headers = headers_dict  # This is from Keys.py, it is a dictionary of the user-agent info and my email
+logger.info(F"Starting bot at {current_time}, {current_day}")
 
 # APIs
 population_api = 'https://www.toontownrewritten.com/api/population'
-invasions_api = 'https://www.toontownrewritten.com/api/invasions'
-silly_meter_api = 'https://www.toontownrewritten.com/api/sillymeter'
-
-
-def send_population_tweet():
-    Time = datetime.datetime.now()
-    tweet_current_time = Time.strftime('%I:%M%p')
-    request = requests.get(url=population_api, params=None, headers=headers)
-    data = request.json()
-    population = data['totalPopulation']  # formats data to only include population data
-    contents = "The current population of Toontown Rewritten at {} is {} toons."
-    tweet_info = contents.format(tweet_current_time, population)
-    print(tweet_info)
-    #api.update_status(tweet_info)  # Comment this out if you want to spam messages for testing
-
-
-def follow_followers():
-    for follower in tweepy.Cursor(api.followers).items():
-        if not follower.following:
-            logger.info(f"Following {follower.name} at {current_time}")
-            follower.follow()
-
-
 scheduler = BlockingScheduler()
-scheduler.add_job(send_population_tweet, 'interval', hours=0.01)  # Sends the automated tweet every 30 minutes
-scheduler.add_job(follow_followers, 'interval', hours=0.1)  # Follows back any followers every 6 minutes
+
+
+class Bot:
+    def __init__(self):
+        global population_list
+        self.get_population()
+        self.run_scheduler()
+        self.tweet_current_time = None
+
+    def get_population(self):
+        Time = datetime.now()
+        self.tweet_current_time = Time.strftime('%I:%M%p')
+        request = requests.get(url=population_api, params=None, headers=headers)
+        data = request.json()
+        self.population = data['totalPopulation']  # formats data to only include population data
+
+        population_list.append(self.population)
+        print(population_list)
+        print(len(population_list))
+
+    @staticmethod
+    def send_population_tweet():
+        contents = f"{current_day} | @Toontown Rewritten had an average population of {round(statistics.mean(population_list))} " \
+            f"toons, with a peak of {max(population_list)} toons and a minimum of {min(population_list)} toons."
+        # load image
+        image = f'Plot {current_day}.png'  # finds the graph we saved
+
+        api.update_with_media(image, contents)  # posts the tweet :)
+
+    @staticmethod
+    def follow_followers():
+        for follower in tweepy.Cursor(api.followers).items():
+            if not follower.following:
+                logger.info(f"Following {follower.name} at {current_time}")
+                follower.follow()
+
+    def run_scheduler(self):
+        scheduler.add_job(self.send_population_tweet, 'interval', hours=24)  # Sends the automated tweet every day
+        scheduler.add_job(self.get_population, 'interval', hours=1.001)  # Sends HTTP GET request to API
+        scheduler.add_job(self.follow_followers, 'interval', hours=0.1)  # Follows back any followers every 6 minutes
+        scheduler.add_job(if_day, 'interval', hours=0.0005)  # Checks if the day has changed
+
+
+def daily_grapher():
+    # Prepare the data
+    y = np.array(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+                  '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'])
+    x = population_list
+    plt.title(f'Population of Toontown every hour on {current_day}')
+    plt.ylabel('Population of Toons online')
+    plt.xlabel('Time (Eastern Daylight (GMT-5))')
+    # Plot the data
+    plt.plot(y, x)
+
+    plt.draw()
+    plt.savefig(f'Plot {current_day}.png', dpi=150)
+    logger.info(F"Drew daily graph at {current_time}, {current_day}")
+
+
+def if_day():
+    global startTime
+    global population_list
+    if startTime.date() != datetime.today().date():
+        print(f'It is now {datetime.today().date()}')
+        daily_grapher()
+        startTime = datetime.today()
+        population_list = []
+    else:
+        return
+
+
+Bot()
 scheduler.start()
 
